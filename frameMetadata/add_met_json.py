5301c786d1f1833b06b2852ef2ec883233388898
Gerald Manipon <pymonger@gmail.com>
2018-03-26 20:31:42 +0000

    merge updates from jpl github

--- frameMetadata/add_met_json.py
+++ frameMetadata/add_met_json.py
@@ -14,7 +14,6 @@ def add_metadata(seed_file, metadata_file):
 
     with open(seed_file) as f:
         seed = json.load(f)
-	seed["account_name"]=default_account_name
         metadata.update(seed)
    
     # overwrite metadata json file
@@ -26,55 +25,23 @@ def add_metadata(seed_file, metadata_file):
     update_misc_data(seed_file, metadata_file) 
 
 
-def add_account_info(in_context_file, in_context_url, metadata_file):
 
+def clean_up(file_name):
     try:
-	print("add_account_name")
-	print(in_context_url)
-
-	home = expanduser("~")
-	netrc_file = os.path.join(home, ".netrc")
-        osaka.main.get(in_context_url, in_context_file)
-        if os.path.isfile(in_context_file):
-            with open(in_context_file, 'r') as fr:
-		seed =json.load(fr)	    
-		if "localize_urls" in seed:
-	    	    datas =seed["localize_urls"]
-            	    for data in datas: 
-	            	if "url" in data:
-		    	    url_path=data["url"].split('/')
-			    print(url_path)
-		    	    ftp_name =url_path[2]
-		    	    print("ftp_name : %s" %ftp_name)
-			    if os.path.isfile(netrc_file):
-                            	with open(netrc_file, 'r') as fr:
-				    for line in fr:
-				    	if ftp_name in  line:
-					    account_name =line.split('login')[1].split('password')[0].strip()
-					    print(account_name)
-		    
-			    		    with open(metadata_file, 'r') as f:
-					    	metadata = json.load(f)
-                                    	    	metadata["account_name"]=account_name
-                                	    with open(metadata_file,'w') as f:
-                                    		json.dump(metadata, f, indent=2, sort_keys=True)
-						print("met file has been updated with account name : %s " %metadata["account_name"])
-						return account_name
-				    print("Account Name : %s NOT found in netrc file " %ftp_name)
-                            else:
-                                print(".netrc file not found")
-                else:
-		    print("localize_url not found in %s " %in_context_url)
-	else:
-	    print("incoming context file not found : %s "%in_context_file)
+	if os.path.isfile(file_name):
+	    os.remove(file_name)
+	    print("Deleted : %s" %file_name)
     except:
-	e = sys.exc_info()[0]
-	print(e)
-    return default_account_name
+        e = sys.exc_info()[0]
+        print(e)
 
 def update_misc_data(seed_file, metadata_file):
 
     try:
+	account_name=None
+	product_path=[]
+	machine_tags=[]
+	tags=[]
         with open(seed_file) as f:
             seed = json.load(f)
 	    datas =seed["job_specification"]["params"]
@@ -87,43 +54,45 @@ def update_misc_data(seed_file, metadata_file):
 		    in_url_path=data["value"].split(file_name)[0]
 		    in_met_file="incoming-"+dest_path[-5]+"-"+dest_path[-4]+"-"+dest_path[-3]+"-"+os.path.splitext(file_name)[0]+".met.json"		    
 		    in_met_url=os.path.join(in_url_path, in_met_file)
-    		    in_context_file="incoming-"+dest_path[-5]+"-"+dest_path[-4]+"-"+dest_path[-3]+"-"+os.path.splitext(file_name)[0]+".context.json"
-   		    in_context_url=os.path.join(in_url_path, in_context_file)
-   		    add_machine_tags(in_met_file, in_met_url, metadata_file)
-		    account_name = add_account_info(in_context_file, in_context_url, metadata_file)
-		    add_machine_tags(in_met_file, in_met_url, metadata_file, account_name)
+                    print(in_met_url)
+		    osaka.main.get(in_met_url, in_met_file)
+        	    if os.path.isfile(in_met_file):
+            		with open(in_met_file, 'r') as fin, open(metadata_file, 'r') as fout:
+                	    seed =json.load(fin)
+			    metadata = json.load(fout)
+                            if "account_name" in seed:
+                                account_name=seed["account_name"]
+                                metadata["account_name"]=account_name
+                                #account_name=unicode(account_name, 'utf_8')
+                                tags = [account_name]
+                	    if "urls" in seed:
+                    		urls =seed["urls"]
+                    		url_path=urls.split('/')
+                    		print(url_path)
+                    		ftp_name =url_path[2]
+                    		product_path=url_path[3:]
+                    		print("get_account_path_info : ftp_name : %s" %ftp_name)
+                    		print("get_account_path_info : product_path : %s" %product_path)
+				tags.extend(x for x in product_path if x not in tags)
+
+			    if "machine_tags" in seed:
+				machine_tags=seed["machine_tags"]
+				tags.extend(x for x in machine_tags if x not in tags)
+
+			    if len(tags)>0:
+				print("tags : %s" %tags)
+				metadata["tags"]=tags
+                        with open(metadata_file,'w') as f:
+            		    json.dump(metadata, f, indent=2, sort_keys=True)
+	
+
+		        clean_up(in_met_file)
+			return
+                    else:
+			print("Incoming Met file NOT found : %s" %in_met_file)
     except:
-        e = sys.exc_info()[0]
+        e = sys.exc_info()
         print(e)
-
-
-def add_machine_tags(in_met_file, in_met_url, metadata_file, account_name=default_account_name):
-	
-    try:
-        print("add_machine_tags")
-        print(in_met_url)
-	account_name=unicode(account_name, 'utf_8')
-        tags = [account_name]
- 	osaka.main.get(in_met_url, in_met_file)
-	if os.path.isfile(in_met_file):
-	    with open(in_met_file, 'r') as fr:
-        	in_meta = json.load(fr)
-		if "tags" in in_meta:
-		    tags.extend(in_meta['tags'])
-		else:
-		    print("tags not found in : %s" %in_met_url)
-	else:
-	    print("Incoming met file not found : " %in_met_file)
-	
-	with open(metadata_file, 'r') as f:
-	    metadata = json.load(f)
-	    metadata["tags"]=tags
-        with open(metadata_file,'w') as f:
-	    json.dump(metadata, f, indent=2, sort_keys=True)
-	    print("Updated Metadata file machine tags with %s" %metadata["tags"])
-    except:
-	e = sys.exc_info()[0]
-	print(e)
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
