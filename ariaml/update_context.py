ffeb4b2f84d7b7f4969f5c6f04bff55a8f5c410d
gmanipon <gmanipon@jpl.nasa.gov>
2018-05-15 17:01:06 +0000

    test with more triage additional globs

--- ariaml/update_context.py
+++ ariaml/update_context.py
@@ -5,7 +5,7 @@ def write_input(ctx_file):
     with open(ctx_file) as f:
         ctx = json.load(f)
 
-    ctx['_triage_additional_globs'] = [ 'S1-IFG*' ]
+    ctx['_triage_additional_globs'] = [ 'S1-IFG*', 'AOI_*', 'celeryconfig.py', 'datasets.json' ]
 
     with open(ctx_file, 'w') as f:
         json.dump(ctx, f, sort_keys=True, indent=2)
