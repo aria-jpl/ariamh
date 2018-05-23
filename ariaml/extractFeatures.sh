a0d1d26a32d15c7009436f15ca50e0bd3471d53a
Gerald Manipon <pymonger@gmail.com>
2018-05-23 23:19:15 +0000

    create stitcher xml input from template pulling URLs from settings

--- ariaml/extractFeatures.sh
+++ ariaml/extractFeatures.sh
@@ -50,15 +50,15 @@ fi
 
 
 echo "##########################################" 1>&2
-echo -n "Copying stitcher.xml: " 1>&2
+echo -n "Creating stitcher.xml: " 1>&2
 date 1>&2
-cp -f $ML_HOME/stitcher.xml . > copy_stitcher.log 2>&1
+/usr/bin/python3 $ML_HOME/createStitcherXml.py stitcher.xml > createStitcherXml.log 2>&1
 STATUS=$?
-echo -n "Finished copying stitcher.xml: " 1>&2
+echo -n "Finished creating stitcher.xml: " 1>&2
 date 1>&2
 if [ $STATUS -ne 0 ]; then
-  echo "Failed to copy stitcher.xml." 1>&2
-  cat copy_stitcher.log 1>&2
+  echo "Failed to create stitcher.xml." 1>&2
+  cat createStitcherXml.log 1>&2
   echo "{}"
   exit $STATUS
 fi
