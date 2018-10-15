7a613294d95fa6f3bce9f44fec8062cec647c4e2
Mohammed Karim <mkarom@jpl.nasa.gov>
2018-10-15 14:11:22 +0000

    update

--- interferogram/sentinel/parpython.py
+++ interferogram/sentinel/parpython.py
@@ -93,7 +93,7 @@ def create_standard_product_job(project, stitched_arg, auto_bbox, ifg_id, master
 
     # set job queue based on project
     job_queue = "%s-job_worker-large" % project
-    job_queue = "factotum-job_worker-small"
+    #job_queue = "factotum-job_worker-small"
     # set localize urls
     localize_urls = [
         { 'url': master_orbit_url },
