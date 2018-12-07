75fcd047e63f77c1313a3a717239ea08efd4dbd6
Gerald Manipon <pymonger@gmail.com>
2018-12-07 07:04:35 +0000

    create water masked standard product and tiles

--- interferogram/sentinel/create_ifg_standard_product.py
+++ interferogram/sentinel/create_ifg_standard_product.py
@@ -9,7 +9,9 @@ import numpy as np
 from datetime import datetime
 from osgeo import ogr
 
+from isceobj.Image.Image import Image
 from utils.UrlUtils_standard_product import UrlUtils
+from utils.imutils import get_image, get_size, crop_mask
 from check_interferogram import check_int
 from create_input_xml_standard_product import create_input_xml
 
@@ -926,9 +928,153 @@ def main():
     # chdir back up to work directory
     os.chdir(cwd)
 
+    # get water mask configuration
+    wbd_url = uu.wbd_url
+    wbd_user = uu.wbd_u
+    wbd_pass = uu.wbd_p
+
+    # get DEM bbox
+    dem_S, dem_N, dem_W, dem_E = bbox
+    dem_S = int(math.floor(dem_S))
+    dem_N = int(math.ceil(dem_N))
+    dem_W = int(math.floor(dem_W))
+    dem_E = int(math.ceil(dem_E))
+
+    # get water mask
+    fp = open('wbdStitcher.xml','w')
+    fp.write('<stitcher>\n')
+    fp.write('    <component name="wbdstitcher">\n')
+    fp.write('        <component name="wbd stitcher">\n')
+    fp.write('            <property name="url">\n')
+    fp.write('                <value>https://urlToRepository</value>\n')
+    fp.write('            </property>\n')
+    fp.write('            <property name="action">\n')
+    fp.write('                <value>stitch</value>\n')
+    fp.write('            </property>\n')
+    fp.write('            <property name="directory">\n')
+    fp.write('                <value>outputdir</value>\n')
+    fp.write('            </property>\n')
+    fp.write('            <property name="bbox">\n')
+    fp.write('                <value>[33,36,-119,-117]</value>\n')
+    fp.write('            </property>\n')
+    fp.write('            <property name="keepWbds">\n')
+    fp.write('                <value>False</value>\n')
+    fp.write('            </property>\n')
+    fp.write('            <property name="noFilling">\n')
+    fp.write('                <value>False</value>\n')
+    fp.write('            </property>\n')
+    fp.write('        </component>\n')
+    fp.write('    </component>\n')
+    fp.write('</stitcher>')
+    fp.close()
+    wbd_file = "wbdmask.wbd"
+    wbd_cmd = [
+        "{}/applications/wbdStitcher.py".format(os.environ['ISCE_HOME']), "wbdStitcher.xml",
+        "wbdstitcher.wbdstitcher.bbox=[{},{},{},{}]".format(dem_S, dem_N, dem_W, dem_E),
+        "wbdstitcher.wbdstitcher.outputfile={}".format(wbd_file),
+        "wbdstitcher.wbdstitcher.url={}".format(wbd_url)
+    ]
+    wbd_cmd_line = " ".join(wbd_cmd)
+    logger.info("Calling wbdStitcher.py: {}".format(wbd_cmd_line))
+    check_call(wbd_cmd_line, shell=True)
+
+    # get product image and size info
+    vrt_prod = get_image("{}/merged/filt_topophase.unw.geo.xml".format(prod_dir))
+    vrt_prod_size = get_size(vrt_prod)
+
+    # get water mask image and size info
+    wbd_xml = "{}.xml".format(wbd_file)
+    wmask = get_image(wbd_xml)
+    wmask_size = get_size(wmask)
+
+    # determine downsample ratio and dowsample water mask
+    lon_rat = 1./(vrt_prod_size['lon']['delta']/wmask_size['lon']['delta'])*100
+    lat_rat = 1./(vrt_prod_size['lat']['delta']/wmask_size['lat']['delta'])*100
+    print("lon_rat/lat_rat: {} {}".format(lon_rat, lat_rat))
+    wbd_ds_file = "wbdmask_ds.wbd"
+    wbd_ds_vrt = "wbdmask_ds.vrt"
+    check_call("gdal_translate -of ENVI -outsize {}% {}% {} {}".format(lon_rat, lat_rat, wbd_file, wbd_ds_file), shell=True)
+    check_call("gdal_translate -of VRT {} {}".format(wbd_ds_file, wbd_ds_vrt), shell=True)
+
+    # update xml file for downsampled water mask
+    wbd_ds_json = "{}.json".format(wbd_ds_file)
+    check_call("gdalinfo -json {} > {}".format(wbd_ds_file, wbd_ds_json), shell=True)
+    with open(wbd_ds_json) as f:
+        info = json.load(f)
+    with open(wbd_xml) as f:
+        doc = parse(f)
+    wbd_ds_xml = "{}.xml".format(wbd_ds_file)
+    doc.xpath('.//component[@name="coordinate1"]/property[@name="delta"]/value')[0].text = str(info['geoTransform'][1])
+    doc.xpath('.//component[@name="coordinate1"]/property[@name="size"]/value')[0].text = str(info['size'][0])
+    doc.xpath('.//component[@name="coordinate2"]/property[@name="delta"]/value')[0].text = str(info['geoTransform'][5])
+    doc.xpath('.//component[@name="coordinate2"]/property[@name="size"]/value')[0].text = str(info['size'][1])
+    doc.xpath('.//property[@name="width"]/value')[0].text = str(info['size'][0])
+    doc.xpath('.//property[@name="length"]/value')[0].text = str(info['size'][1])
+    doc.xpath('.//property[@name="metadata_location"]/value')[0].text = wbd_ds_xml
+    doc.xpath('.//property[@name="file_name"]/value')[0].text = wbd_ds_file
+    for rm in doc.xpath('.//property[@name="extra_file_name"]'): rm.getparent().remove(rm)
+    doc.write(wbd_ds_xml)
+
+    # get downsampled water mask image and size info
+    wmask_ds = get_image(wbd_ds_xml)
+    wmask_ds_size = get_size(wmask_ds)
+
+    print("vrt_prod.filename: {}".format(vrt_prod.filename))
+    print("vrt_prod.bands: {}".format(vrt_prod.bands))
+    print("vrt_prod size: {}".format(vrt_prod_size))
+    print("wmask.filename: {}".format(wmask.filename))
+    print("wmask.bands: {}".format(wmask.bands))
+    print("wmask size: {}".format(wmask_size))
+    print("wmask_ds.filename: {}".format(wmask_ds.filename))
+    print("wmask_ds.bands: {}".format(wmask_ds.bands))
+    print("wmask_ds size: {}".format(wmask_ds_size))
+
+    # crop the downsampled water mask
+    wbd_cropped_file = "wbdmask_cropped.wbd"
+    wmask_cropped = crop_mask(vrt_prod, wmask_ds, wbd_cropped_file)
+    print("wmask_cropped shape: {}".format(wmask_cropped.shape))
+
+    # mask out water from the product data
+    vrt_prod_shape = (vrt_prod_size['lat']['size'], vrt_prod.bands, vrt_prod_size['lon']['size'])
+    vrt_prod_im = np.memmap(vrt_prod.filename, dtype=vrt_prod.toNumpyDataType(),
+                            mode='c', shape=vrt_prod_shape)
+    im1 = vrt_prod_im[:,:,:]
+    for i in range(vrt_prod.bands):
+        im1_tmp = im1[:,i,:]
+        im1_tmp[wmask_cropped == -1] = 0
+
+    # create masked product image
+    masked_filt = "filt_topophase.masked.unw.geo"
+    masked_filt_xml = "filt_topophase.masked.unw.geo.xml"
+    tim = np.memmap(masked_filt, dtype=vrt_prod.toNumpyDataType(), mode='w+', shape=vrt_prod_shape)
+    tim[:,:,:] = im1
+    im  = Image()
+    with open("{}/merged/filt_topophase.unw.geo.xml".format(prod_dir)) as f:
+        doc = parse(f)
+    doc.xpath('.//property[@name="file_name"]/value')[0].text = masked_filt
+    for rm in doc.xpath('.//property[@name="extra_file_name"]'): rm.getparent().remove(rm)
+    doc.write(masked_filt_xml)
+    im.load(masked_filt_xml)
+    latstart = vrt_prod_size['lat']['val']
+    lonstart = vrt_prod_size['lon']['val']
+    latsize = vrt_prod_size['lat']['size']
+    lonsize = vrt_prod_size['lon']['size']
+    latdelta = vrt_prod_size['lat']['delta']
+    londelta = vrt_prod_size['lon']['delta']
+    im.coord2.coordStart = latstart
+    im.coord2.coordSize = latsize
+    im.coord2.coordDelta = latdelta
+    im.coord2.coordEnd = latstart + latsize*latdelta
+    im.coord1.coordStart = lonstart
+    im.coord1.coordSize = lonsize
+    im.coord1.coordDelta = londelta
+    im.coord1.coordEnd = lonstart + lonsize*londelta
+    im.filename = masked_filt
+    im.renderHdr()
+
     # create displacement tile layer
     tiles_dir = "{}/tiles".format(prod_dir)
-    vrt_prod_file = "{}/merged/filt_topophase.unw.geo.vrt".format(prod_dir)
+    vrt_prod_file = "filt_topophase.masked.unw.geo.vrt"
     tiler_cmd_path = os.path.abspath(os.path.join(BASE_PATH, '..', '..', 'map_tiler'))
     dis_layer = "displacement"
     tiler_cmd_tmpl = "{}/create_tiles.py {} {}/{} -b 2 -m prism --nodata 0"
@@ -948,6 +1094,10 @@ def main():
     check_call(cog_cmd_tmpl.format(cog_prod_file), shell=True)
     os.unlink("tmp.tif")
 
+    # move masked standard product to product directory
+    shutil.move(masked_filt, prod_dir)
+    shutil.move(masked_filt_xml, prod_dir)
+    shutil.move(vrt_prod_file, prod_dir)
 
     # extract metadata from master
     met_file = os.path.join(prod_dir, "{}.met.json".format(id))
