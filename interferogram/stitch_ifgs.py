1cb4a1a50dd822de3d6f178845375906644a6920
Gerald Manipon <pymonger@gmail.com>
2019-02-25 23:45:25 +0000

    remove amplitude layer; rename displacement to interferogram

--- interferogram/stitch_ifgs.py
+++ interferogram/stitch_ifgs.py
@@ -112,7 +112,7 @@ def create_met_json(id, version, env, starttime, endtime, met_files, met_json_fi
         'track_number': None,
         'archive_filename': id,
         'dataset_type': 'slc',
-        'tile_layers': [ 'amplitude', 'displacement' ],
+        'tile_layers': [ 'amplitude', 'interferogram' ],
         'latitude_index_min': int(math.floor(env[2] * 10)),
         'latitude_index_max': int(math.ceil(env[3] * 10)),
         'parallel_baseline': [],
@@ -348,14 +348,14 @@ def main():
     tiles_dir = "{}/tiles".format(dataset_dir)
     vrt_prod_file = "{}/filt_topophase.unw.geo.vrt".format(dataset_dir)
     tiler_cmd_path = os.path.abspath(os.path.join(BASE_PATH, '..', 'map_tiler'))
-    dis_layer = "displacement"
+    dis_layer = "interferogram"
     tiler_cmd_tmpl = "{}/create_tiles.py {} {}/{} -b 2 -m prism --nodata 0"
     call_noerr(tiler_cmd_tmpl.format(tiler_cmd_path, vrt_prod_file, tiles_dir, dis_layer))
 
     # create amplitude tile layer
-    amp_layer = "amplitude"
-    tiler_cmd_tmpl = "{}/create_tiles.py {} {}/{} -b 1 -m gray --clim_min 10 --clim_max_pct 80 --nodata 0"
-    call_noerr(tiler_cmd_tmpl.format(tiler_cmd_path, vrt_prod_file, tiles_dir, amp_layer))
+    #amp_layer = "amplitude"
+    #tiler_cmd_tmpl = "{}/create_tiles.py {} {}/{} -b 1 -m gray --clim_min 10 --clim_max_pct 80 --nodata 0"
+    #call_noerr(tiler_cmd_tmpl.format(tiler_cmd_path, vrt_prod_file, tiles_dir, amp_layer))
 
     # create COG (cloud optimized geotiff) with no_data set
     cog_prod_file = "{}/filt_topophase.unw.geo.tif".format(dataset_dir)
