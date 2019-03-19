bf1907ac42c11d36b453761f53ee0b875dd79460
Mohammed Karim <mkarom@jpl.nasa.gov>
2019-03-19 18:35:43 +0000

    update

--- frameMetadata/sling_extract.py
+++ frameMetadata/sling_extract.py
@@ -6,12 +6,16 @@ the product and leveraging the configured metadata extractor defined
 for the product in datasets JSON config.
 """
 
-import os, sys, re, json, shutil, requests, logging, traceback, argparse
+import os, sys, re, hashlib, json, shutil, requests, logging, traceback, argparse
 from subprocess import check_output
 import time
-
+import tarfile, zipfile
 from hysds.recognize import Recognizer
 import osaka.main
+from atomicwrites import atomic_write
+import hysds
+from hysds.log_utils import logger, log_prov_es
+from hysds.celery import app
 
 SCRIPT_RE = re.compile(r'script:(.*)$')
 
@@ -29,6 +33,108 @@ ALL_TYPES.extend(TAR_TYPE)
 
 log_format = "[%(asctime)s: %(levelname)s/%(funcName)s] %(message)s"
 logging.basicConfig(format=log_format, level=logging.INFO)
+
+def get_download_params(url):
+    """Set osaka download params."""
+
+    params = {}
+
+    # set profile
+    for prof in app.conf.get('BUCKET_PROFILES', []):
+        if 'profile_name' in params: break
+        if prof.get('bucket_patterns', None) is None:
+            params['profile_name'] = prof['profile']
+            break
+        else:
+            if isinstance(prof['bucket_patterns'], list):
+                bucket_patterns = prof['bucket_patterns']
+            else: bucket_patterns = [ prof['bucket_patterns'] ]
+            for bucket_pattern in prof['bucket_patterns']:
+                regex = re.compile(bucket_pattern)
+                match = regex.search(url)
+                if match:
+                    logger.info("{} matched '{}' for profile {}.".format(url, bucket_pattern, prof['profile']))
+                    params['profile_name'] = prof['profile']
+                    break
+                
+    return params
+
+ 
+def download_file(url, path, cache=False):
+    """Download file/dir for input."""
+
+    params = get_download_params(url)
+    if cache:
+        url_hash = hashlib.md5(url).hexdigest()
+        hash_dir = os.path.join(app.conf.ROOT_WORK_DIR, 'cache', *url_hash[0:4])
+        cache_dir = os.path.join(hash_dir, url_hash)
+        makedirs(cache_dir)
+        signal_file = os.path.join(cache_dir, '.localized')
+        if os.path.exists(signal_file):
+            logger.info("cache hit for {} at {}".format(url, cache_dir))
+        else:
+            logger.info("cache miss for {}".format(url))
+            try: osaka.main.get(url, cache_dir, params=params)
+            except Exception, e:
+                shutil.rmtree(cache_dir)
+                tb = traceback.format_exc()
+                raise(RuntimeError("Failed to download %s to cache %s: %s\n%s" % \
+                    (url, cache_dir, str(e), tb)))
+            with atomic_write(signal_file, overwrite=True) as f:
+                f.write("%sZ\n" % datetime.utcnow().isoformat())
+        for i in os.listdir(cache_dir):
+            if i == '.localized': continue
+            cached_obj = os.path.join(cache_dir, i)
+            if os.path.isdir(cached_obj):
+                dst = os.path.join(path, i) if os.path.isdir(path) else path
+                try: os.symlink(cached_obj, dst)
+                except:
+                    logger.error("Failed to soft link {} to {}".format(cached_obj, dst))
+                    raise
+            else:
+                try: os.symlink(cached_obj, path)
+                except:
+                    logger.error("Failed to soft link {} to {}".format(cached_obj, path))
+                    raise
+    else: return osaka.main.get(url, path, params=params)
+
+
+def localize_file(url, path, cache):
+    """Localize urls for job inputs. Track metrics."""
+
+    # get job info
+    job_dir = job['job_info']['job_dir']
+
+    # localize urls
+    if path is None: path = '%s/' % job_dir
+    else:
+        if path.startswith('/'): pass
+        else: path = os.path.join(job_dir, path)
+    if os.path.isdir(path) or path.endswith('/'):
+        path = os.path.join(path, os.path.basename(url))
+    dir_path = os.path.dirname(path)
+    makedirs(dir_path)
+    loc_t1 = datetime.utcnow()
+    try: download_file(url, path, cache=cache)
+    except Exception, e:
+        tb = traceback.format_exc()
+        raise(RuntimeError("Failed to download %s: %s\n%s" % (url, str(e), tb)))
+    loc_t2 = datetime.utcnow()
+    loc_dur = (loc_t2 - loc_t1).total_seconds()
+    path_disk_usage = get_disk_usage(path)
+    job['job_info']['metrics']['inputs_localized'].append({
+        'url': url,
+        'path': path,
+        'disk_usage': path_disk_usage,
+        'time_start': loc_t1.isoformat() + 'Z',
+        'time_end': loc_t2.isoformat() + 'Z',
+        'duration': loc_dur,
+        'transfer_rate': path_disk_usage/loc_dur
+        })
+
+    # signal run_job() to continue
+    return True
+
 
 def verify(path, file_type):
     """Verify downloaded file is okay by checking that it can
@@ -273,7 +379,9 @@ if __name__ == "__main__":
         filename, file_extension = os.path.splitext(args.file)
         logging.info("localize_url : %s \nfile : %s" %(localize_url, args.file))
        
-        sling(localize_url, filename, args.prod_name, "zip", args.prod_date)
+        localize_file(localize_url, args.file, True)
+
+        #sling(localize_url, filename, args.prod_name, "zip", args.prod_date)
         #, prod_met=None, oauth_url=None, force=False, force_extract=False)
 
         '''
