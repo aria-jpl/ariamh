822202495371afb7679aa9f12dec0270082f40ce
Mohammed Karim <mkarom@jpl.nasa.gov>
2019-03-28 20:53:55 +0000

    passing log error

--- frameMetadata/sling_extract_asf.py
+++ frameMetadata/sling_extract_asf.py
@@ -293,6 +293,19 @@ def localize_file(url, path, cache):
     # signal run_job() to continue
     return True
 
+def get_log_err(log_file):
+    err_msg = None
+    try:
+        with open(log_file, 'r') as f:
+            last = None
+            for last in (line for line in f if line.rstrip('\n')):
+                pass
+            if last:
+                err_msg = last
+    except Exception as err:
+        logging.info("Error reading %s : %s" %(log_file, str(err))
+
+    return err_msg      
 
 def run_extractor(dsets_file, prod_path, url, ctx):
     """Run extractor configured in datasets JSON config."""
@@ -344,7 +357,21 @@ def run_extractor(dsets_file, prod_path, url, ctx):
         try:
             m = check_output([extractor, prod_path])
         except CalledProcessError as e:
-            raise RuntimeError("command '{}' return with error (code {}): {}".format(e.cmd, e.returncode, e))
+            err_msg = e.out
+            root_dir = os.path.abspath(os.path.join(os.getcwd(), '..'))
+            prov_log = os.path.join(root_dir, 'create_prov_es.log')
+            split_log  = os.path.join(root_dir, 'split_swath_products.log')
+            loggere.info("%s\n%s" %(prov_log, split_log))
+            if prov_log.is_file():
+                prov_err = get_log_err(prov_log):
+                if prov_err:
+                    err_msg = prov_err
+            elif split_log.is_file():
+                split_err = get_log_err(split_log)
+                    if split_err:
+                        err_msg=split_err
+             
+            raise RuntimeError("command '{}' return with error (code {}): {}".format(e.cmd, e.returncode, err_msg))
 
         if os.path.exists(metadata_file):
             with open(metadata_file) as f:
