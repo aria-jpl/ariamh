f066e334a87aee4d24021c0d7ff99230604df3e5
Mohammed Karim <mkarim@jpl.nasa.gov>
2019-05-06 16:54:13 +0000

    revert back +-4

--- interferogram/sentinel/create_standard_product_s1.py
+++ interferogram/sentinel/create_standard_product_s1.py
@@ -992,11 +992,11 @@ def main():
             if dem_type == "NED13-downsampled": downsample_option = "-d 33%"
             else: downsample_option = ""
  
-            
-            dem_S = dem_S - 4 if dem_S > -86 else dem_S
-            dem_N = dem_N + 4 if dem_N < 86 else dem_N
-            dem_W = dem_W - 4 if dem_W > -176 else dem_W
-            dem_E = dem_E + 4 if dem_E < 176 else dem_E
+           
+            dem_S = dem_S - 1 if dem_S > -89 else dem_S
+            dem_N = dem_N + 1 if dem_N < 89 else dem_N
+            dem_W = dem_W - 1 if dem_W > -179 else dem_W
+            dem_E = dem_E + 1 if dem_E < 179 else dem_E
             '''
             dem_S, dem_N, dem_W, dem_E = bbox
             dem_S = int(math.floor(dem_S))
