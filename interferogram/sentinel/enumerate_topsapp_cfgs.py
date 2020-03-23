e938e9e8f41a06d3f0fc37f0fcf3a763e46918f9
Gerald Manipon <pymonger@gmail.com>
2020-03-23 23:36:41 +0000

    encode before hashing

--- interferogram/sentinel/enumerate_topsapp_cfgs.py
+++ interferogram/sentinel/enumerate_topsapp_cfgs.py
@@ -775,7 +775,7 @@ def get_topsapp_cfgs(context_file, temporalBaseline=72, id_tmpl=IFG_ID_TMPL, min
                                     context['range_looks'],
                                     context['filter_strength'],
                                     context.get('dem_type', 'SRTM+v3'),
-                                ])).hexdigest()
+                                ]).encode()).hexdigest()
                                 ifg_ids.append(id_tmpl.format('M', len(ref_ids), len(matched_ids),
                                                               track, ifg_master_dt,
                                                               ifg_slave_dt, swathnum,
@@ -810,7 +810,7 @@ def get_topsapp_cfgs(context_file, temporalBaseline=72, id_tmpl=IFG_ID_TMPL, min
                                     context['range_looks'],
                                     context['filter_strength'],
                                     context.get('dem_type', 'SRTM+v3'),
-                                ])).hexdigest()
+                                ]).encode()).hexdigest()
                                 ifg_ids.append(id_tmpl.format('S', len(matched_ids), len(ref_ids),
                                                               track, ifg_master_dt,
                                                               ifg_slave_dt, swathnum,
@@ -882,7 +882,7 @@ def get_topsapp_cfgs(context_file, temporalBaseline=72, id_tmpl=IFG_ID_TMPL, min
                                     context['range_looks'],
                                     context['filter_strength'],
                                     context.get('dem_type', 'SRTM+v3'),
-                                ])).hexdigest()
+                                ]).encode()).hexdigest()
                                 ifg_ids.append(id_tmpl.format('S', len(matched_ids), len(ref_ids),
                                                               track, ifg_master_dt,
                                                               ifg_slave_dt, swathnum,
