d4f4adfef59edccc5cb6c1fc78037986bef6b76a
Mohammed Karim <mkarim@jpl.nasa.gov>
2020-04-05 20:30:51 +0000

    insarzd

--- docker/setup.sh
+++ docker/setup.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
 # clone spyddder-man to be moved to its final location by docker builder
-git clone -b standard-product https://github.com/mkarim2017/spyddder-man.git
+git clone -b https://github.com/CunrenLiang/insarzd
 
-# clone slcp2pm to be moved to its final location by docker builder
-git clone https://github.com/hysds/slcp2pm.git
-
-# clone slcp2cor to be moved to its final location by docker builder
-git clone https://github.com/hysds/slcp2cor.git
