604e88005806c51f8866db99e45acbba9e797e38
Mohammed Karim <mkarim@jpl.nasa.gov>
2020-04-15 13:10:22 +0000

    remove insarzd

--- docker/setup.sh
+++ docker/setup.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# clone insarzd to be moved to its final location by docker builder
-git clone -b master https://github.com/CunrenLiang/insarzd
-
 # clone alos2 ingest
 git clone -b master https://github.com/earthobservatory/alos2-ingest.git
 
