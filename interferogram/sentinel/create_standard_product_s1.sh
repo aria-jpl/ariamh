d5f3f31f60572687d8b3a4d11de689c3dd7fa66b
Mohammed Karim <widewidesea2017@gmail.com>
2020-05-12 09:00:56 -0700

    removed giant

--- interferogram/sentinel/create_standard_product_s1.sh
+++ interferogram/sentinel/create_standard_product_s1.sh
@@ -6,11 +6,11 @@ BASE_PATH=$(cd "${BASE_PATH}"; pwd)
 export GMT_HOME=/usr/local/gmt
 export ARIAMH_HOME=$HOME/ariamh
 source $ARIAMH_HOME/isce.sh
-source $ARIAMH_HOME/giant.sh
+#source $ARIAMH_HOME/giant.sh
 export TROPMAP_HOME=$HOME/tropmap
 export UTILS_HOME=$ARIAMH_HOME/utils
-export GIANT_HOME=/usr/local/giant/GIAnT
-export PYTHONPATH=.:$ISCE_HOME/applications:$ISCE_HOME/components:$BASE_PATH:$ARIAMH_HOME:$TROPMAP_HOME:$GIANT_HOME:$PYTHONPATH
+#export GIANT_HOME=/usr/local/giant/GIAnT
+export PYTHONPATH=.:$ISCE_HOME/applications:$ISCE_HOME/components:$BASE_PATH:$ARIAMH_HOME:$TROPMAP_HOME:$PYTHONPATH
 export PATH=$BASE_PATH:$TROPMAP_HOME:$GMT_HOME/bin:$PATH
 
 # source environment
