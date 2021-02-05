f7b437c7be54b3666fa4e5e3cd9b95a6d8b3e51a
Charlie Marshak <cmarshak@users.noreply.github.com>
2021-02-04 18:44:25 -0800

    Update Path to include ISCE Applicatoins

--- interferogram/sentinel/create_standard_product_s1.sh
+++ interferogram/sentinel/create_standard_product_s1.sh
@@ -11,7 +11,7 @@ export TROPMAP_HOME=$HOME/tropmap
 export UTILS_HOME=$ARIAMH_HOME/utils
 #export GIANT_HOME=/usr/local/giant/GIAnT
 export PYTHONPATH=.:$ISCE_HOME/applications:$ISCE_HOME/components:$BASE_PATH:$ARIAMH_HOME:$TROPMAP_HOME:$PYTHONPATH
-export PATH=$BASE_PATH:$TROPMAP_HOME:$GMT_HOME/bin:$PATH
+export PATH=$BASE_PATH:$TROPMAP_HOME:$ISCE_HOME/applications:$GMT_HOME/bin:$PATH
 
 # source environment
 source $HOME/verdi/bin/activate
